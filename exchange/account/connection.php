720cb31aaa4b56f2b87340d0741f7c1596edc7a2
Michael <eramehmichael2000@gmail.com>
2025-09-04 19:54:32 +0100

    update

--- exchange/account/connection.php
+++ exchange/account/connection.php
@@ -1,9 +1,11 @@
 <?php
 // Database connection (Change credentials accordingly)
         $servername = "s1099.usc1.mysecurecloudhost.com";
-        $username = "dualstre_octavat";
+        //$username = "dualstre_octavat";
+        $username = "dualxtra_octavat";
         $password = "OCT3244@DSfdrdv";
-        $dbname = "dualstre_octavat";
+        //$dbname = "dualstre_octavat";
+        $dbname = "dualxtra_octavat";
 
         $conn = new mysqli($servername, $username, $password, $dbname);
         // if($conn){
