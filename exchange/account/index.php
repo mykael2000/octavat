9c2c957a7d903ef101985c9427202df01eeab340
Michael <eramehmichael2000@gmail.com>
2025-09-08 23:10:09 +0100

    add

--- exchange/account/index.php
+++ exchange/account/index.php
@@ -472,15 +472,17 @@ include('function.php');
         const menuBtn = document.getElementById('menu-btn');
         const sidebar = document.getElementById('sidebar');
         const overlay = document.getElementById('overlay');
+        const closeMenuBtn = document.getElementById('close-menu-btn');
 
         function toggleMenu() {
-            sidebar.classList.toggle('-translate-x-full');
+            sidebar.classList.toggle('translate-x-full');
             overlay.classList.toggle('hidden');
         }
 
         // Event listeners for the menu button and overlay
         menuBtn.addEventListener('click', toggleMenu);
         overlay.addEventListener('click', toggleMenu);
+        closeMenuBtn.addEventListener('click', toggleMenu);
     </script>
 </body>
 </html>
