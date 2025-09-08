12bfd25b6aad94f7a752aae670de9a37894f6cfa
Michael <eramehmichael2000@gmail.com>
2025-09-08 23:52:29 +0100

    add

--- exchange/account/index.php
+++ exchange/account/index.php
@@ -31,7 +31,7 @@ include('function.php');
     <div class="min-h-screen flex flex-col">
          <nav id="sidebar" class="fixed top-0 right-0 w-full h-full bg-[#121414] border-l border-gray-700/50 z-50 transform translate-x-full transition-transform duration-300 ease-in-out">
             <div class="p-4 flex flex-col h-full">
-                 <div class="flex justify-end mb-8">
+                 <div style="float: right" class="flex justify-end mb-8">
                     <button id="close-menu-btn" class="p-2 rounded-full hover:bg-gray-800 focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
