cbc21547d14a5ede841bd84ed755b947c1a15c16
Michael <eramehmichael2000@gmail.com>
2025-09-10 00:49:17 +0100

    add

--- exchange/account/topnav.php
+++ exchange/account/topnav.php
@@ -83,9 +83,7 @@
 <header class="p-4 flex items-center justify-between bg-[#121417] border-b border-gray-700/50">
     <div class="flex items-center space-x-2">
         <a href="profile.php" class="p-2 rounded-full hover:bg-gray-800 focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mb-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7A4 4 0 1112 3a4 4 0 014 4zM12 14c-4.418 0-8 3.582-8 8H20c0-4.418-3.582-8-8-8z" />
-            </svg>
+            <img src="profile.jpg" height="30px" width="30px">
         </a>
     </div>
     <div class="flex items-center space-x-2">
