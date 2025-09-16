c868e2c54214a2f4184aaf17bf80887cb5113f91
Michael <eramehmichael2000@gmail.com>
2025-09-16 20:38:35 +0100

    update:

---
+++ about.php
@@ -0,0 +1,116 @@
+<?php include("header.php"); ?>
+<div class="relative bg-gradient-to-br from-blue-950 to-indigo-950 text-white py-16 lg:py-32 overflow-hidden">
+    <img src="images/blob.svg" alt="" class="absolute inset-0 w-full h-full object-cover opacity-20">
+    <div class="container mx-auto px-4 relative z-10">
+        <div class="flex flex-col items-center justify-center text-center pt-5">
+            <div class="lg:w-8/12">
+                <h1 class="text-4xl lg:text-5xl font-bold mb-3 text-blue-900 dark:text-gray-100" data-aos="fade-up">About Us</h1>
+                <p class="text-sm lg:text-base text-gray-200 dark:text-gray-300">
+                    <span class="text-gray-200 dark:text-gray-300">
+                        Octavat is revolutionizing how people invest and trade. The Octavat platform allows users to deploy automated trading bots to execute non-stop cryptocurrency trading and minimize risks. Committed to building a reliable platform that helps users stabilize their crypto earnings and remove the proclivity of emotional decision-making, Octavat embraces user feedback to drive refinements, grow the platform, and develop it into multiple exceptional products. Priding ourselves on our Octavat ecosystem, we provide users with a place to learn, share and build together. We are extremely proud of our sensitivity to user-generated feedback, as it allows us to remain customer-centric and ensures that all our developments are designed specifically for our Octavat family!
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="relative z-0 py-16 bg-gray-900">
+    <div class="flex flex-col lg:flex-row items-stretch">
+        <div class="flex-1 p-8 md:p-16 bg-blue-950">
+            <h2 class="text-3xl lg:text-4xl font-bold text-white mb-3 text-center">Our Mission</h2>
+            <p class="mb-5 text-gray-300 text-center">
+                Octavat’s mission is to create the financial infrastructure of the future. We build crypto-financial infrastructure based on cryptocurrency. Our suite of technologies enhances the Bitcoin ecosystem, making it easier for individuals and businesses with zero trading experience to successfully make a profit. The most robust and secure. Applying cutting-edge cryptography and security engineering.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="bg-gray-800 py-16">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div class="flex justify-center mb-8 col-span-full">
+                <h3 class="text-3xl font-bold text-white">Core Values</h3>
+            </div>
+            
+            <div class="col-span-1" data-aos="fade-up" data-aos-delay="0">
+                <div class="flex flex-col p-6 rounded-lg shadow-lg bg-gray-700 h-full">
+                    <div>
+                        <h3 class="text-xl font-bold text-white mb-2">Don’t trust. Verify</h3>
+                        <p class="text-gray-300">We believe in empowering individuals and businesses to take control of their own finances and reducing the need for trusted third parties.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-1" data-aos="fade-up" data-aos-delay="100">
+                <div class="flex flex-col p-6 rounded-lg shadow-lg bg-gray-700 h-full">
+                    <div>
+                        <h3 class="text-xl font-bold text-white mb-2">Open finance for everyone.</h3>
+                        <p class="text-gray-300">We build software that accelerates the adoption of Bitcoin and peer-to-peer finance for a fairer financial system that benefits everyone, not just a privileged few.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-1" data-aos="fade-up" data-aos-delay="100">
+                <div class="flex flex-col p-6 rounded-lg shadow-lg bg-gray-700 h-full">
+                    <div>
+                        <h3 class="text-xl font-bold text-white mb-2">Advantages of Octavat Automatic Trading</h3>
+                        <p class="text-gray-300">
+                            Cryptocurrency trading is considered one of the premiere markets to trade, and an automated cryptocurrency trading system can help by instantly executing all crypto transactions. With the help of our software, traders will only have to switch on the computer and let our software take care of placing trades.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-1" data-aos="fade-up" data-aos-delay="100">
+                <div class="flex flex-col p-6 rounded-lg shadow-lg bg-gray-700 h-full">
+                    <div>
+                        <h3 class="text-xl font-bold text-white mb-2">Automatic Trading Is Emotionless</h3>
+                        <p class="text-gray-300">
+                            Arguably the largest and most obvious advantage of using Octavat automated trading software is the removal of human emotion. Octavat is programmed to enter and exit positions in the financial markets once certain criteria have been met, removing any emotion (negative or otherwise) from the decision-making process.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-1" data-aos="fade-up" data-aos-delay="100">
+                <div class="flex flex-col p-6 rounded-lg shadow-lg bg-gray-700 h-full">
+                    <div>
+                        <h3 class="text-xl font-bold text-white mb-2">Discipline and Consistency</h3>
+                        <p class="text-gray-300">
+                            This allows traders to keep their discipline and stick to their trading plan, whatever that might be, as the system will ensure it is followed precisely and consistently. This means that traders do not have the opportunity to hesitate and question a particular trade due to fear of taking a loss.
+                            <br><br>
+                            It is important to understand that there is no trading plan or strategy which will win all the time. Losses are part of trading and it goes without saying that they can be emotionally draining. Octavat has no emotion and, therefore, losses will not lead to a deviation from the overall plan.
+                            <br><br>
+                            With the same logic, Octavat prevents traders from overtrading, entering trades that do not fit within their plan or holding out for extra profit through greed.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-1" data-aos="fade-up" data-aos-delay="100">
+                <div class="flex flex-col p-6 rounded-lg shadow-lg bg-gray-700 h-full">
+                    <div>
+                        <h3 class="text-xl font-bold text-white mb-2">Accessibility</h3>
+                        <p class="text-gray-300">
+                            Octavat software can be, and is, utilised by beginners and professionals alike, both of whom may find these systems helpful with decision-making.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-1" data-aos="fade-up" data-aos-delay="100">
+                <div class="flex flex-col p-6 rounded-lg shadow-lg bg-gray-700 h-full">
+                    <div>
+                        <h3 class="text-xl font-bold text-white mb-2">Order Entry Speed</h3>
+                        <p class="text-gray-300">
+                            The financial markets can move quickly, therefore, the speed at which a position is entered or exited can have a big impact on the success of a trade. Octavat responds immediately and places orders (complete with stop losses and take profits) as soon as the predetermined criteria are satisfied.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<?php include("footer.php"); ?>
