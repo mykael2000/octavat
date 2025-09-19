f6fc740fbe3542a2aaa2060cf2da76970d31cd0b
Michael <eramehmichael2000@gmail.com>
2025-09-19 14:41:36 +0100

    update:

--- header.php
+++ header.php
@@ -126,9 +126,9 @@
                 <button class="text-gray-400 hover:text-white transition-colors">
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L14 11.586V8a6 6 0 00-6-6zm-2 10.414V8a2 2 0 114 0v4.414l-2 2-2-2z"></path></svg>
                 </button>
-                <button class="bg-gray-800 rounded-full w-8 h-8 flex items-center justify-center text-gray-400 hover:bg-gray-700 transition-colors">
+                <a href="login.php" class="bg-gray-800 rounded-full w-8 h-8 flex items-center justify-center text-gray-400 hover:bg-gray-700 transition-colors">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
-                </button>
+                </a>
                 <div class="relative">
                     <button class="bg-gray-800 rounded-full w-8 h-8 flex items-center justify-center text-gray-400 hover:bg-gray-700 transition-colors">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L14 11.586V8a6 6 0 00-6-6zm-2 10.414V8a2 2 0 114 0v4.414l-2 2-2-2z"></path></svg>
