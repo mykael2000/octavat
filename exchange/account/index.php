cbd83c8faa2d45d66103934152eb11ec1d86b5c9
Michael <eramehmichael2000@gmail.com>
2025-09-19 14:43:19 +0100

    update:

--- exchange/account/index.php
+++ exchange/account/index.php
@@ -284,7 +284,7 @@ include('function.php');
                 <section id="tapper-section" class="container mx-auto px-4">
                 <h2 class="text-center text-2xl font-bold mb-4">Tap to Claim Your Free Octavat Token!</h2>
                 <div id="coin-container">
-                    <img height="50px" width="50px" id="coin-image" src="logo.png" alt="Tappable Coin">
+                    <img height="50px" width="50px" id="coin-image" src="../../logo.png" alt="Tappable Coin">
                 </div>
                 <div id="tap-count" class="mt-4 text-xl font-bold">Taps: <span id="taps">0</span></div>
 
