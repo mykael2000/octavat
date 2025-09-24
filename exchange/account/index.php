d1064acf652760d402e69473ded2720cc4ab5b2a
Michael <eramehmichael2000@gmail.com>
2025-09-24 20:28:40 +0100

    update:

--- exchange/account/index.php
+++ exchange/account/index.php
@@ -458,15 +458,15 @@ include('function.php');
                         <div class="flex items-center space-x-4">
                             <span class="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
-                                <?php echo $followers; ?>
+                                <?php echo $article['followers']; ?>
                             </span>
                             <span class="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" /></svg>
-                                <?php echo $upvotes; ?>
+                                <?php echo $article['upvotes']; ?>
                             </span>
                             <span class="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" /></svg>
-                                <?php echo $downvotes; ?>
+                                <?php echo $article['downvotes']; ?>
                             </span>
                         </div>
                     </div>
