fc706f4772b1e30ce90106d58d4d7791af5341f7
Michael <eramehmichael2000@gmail.com>
2025-09-24 21:49:36 +0100

    update:

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -425,13 +425,7 @@ $query = mysqli_query($conn, $sql);
 
                         </a>
                     </li>
-                    <li class="treeview">
-                        <a href="bnbwallet.php">
-                            <i class="fa fa-files-o"></i>
-                            <span>Edit BNB Wallet</span>
-
-                        </a>
-                    </li>
+                    
 
 
 
