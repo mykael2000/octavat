5dfdd1189fbb60d7fc7473d9bd9d5c089715cb1c
Michael <eramehmichael2000@gmail.com>
2025-09-24 20:51:00 +0100

    update:

--- exchange/account/index.php
+++ exchange/account/index.php
@@ -459,15 +459,21 @@ include('function.php');
                     </div>
 
                     <div class="flex justify-between items-center text-gray-400 text-sm">
-                        <div class="flex items-end space-x-4">
-                            
+                        <div class="flex items-center space-x-2">
                             <span class="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" /></svg>
-                                <?php echo $article['upvotes']; ?>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.636a2 2 0 01-1.789-2.894l3.5-7zM7 9H2.236a2 2 0 00-1.789 2.894l3.5 7A2 2 0 008.736 21h4.636a2 2 0 001.789-2.894l-3.5-7z" />
+                                </svg>
+                                <span class="text-white">Upvotes: <?php echo $article['upvotes']; ?></span>
                             </span>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
                             <span class="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" /></svg>
-                                <?php echo $article['downvotes']; ?>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10 14L5.236 7.106a2 2 0 013.528-2.212l3.236 5.163zM14 14l4.764-6.894a2 2 0 00-3.528-2.212l-3.236 5.163z" />
+                                </svg>
+                                <span class="text-white">Downvotes: <?php echo $article['downvotes']; ?></span>
                             </span>
                         </div>
                     </div>
