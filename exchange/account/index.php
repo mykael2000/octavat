cf7966f7f2b4815138d9c2b558ead7fb563b48a1
Michael <eramehmichael2000@gmail.com>
2025-09-24 20:38:00 +0100

    update:

--- exchange/account/index.php
+++ exchange/account/index.php
@@ -441,7 +441,11 @@ include('function.php');
                                     <svg class="w-4 h-4 text-blue-500 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                                     <span class="text-sm text-gray-500 ml-2"><?php echo $time_ago; ?></span>
                                 </div>
-                                <button class="text-sm font-semibold text-green-accent">+ Follow</button>
+                                <button class="text-sm font-semibold text-green-accent">
+                                    <span class="flex items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
+                                        <?php echo $article['followers']; ?>
+                                    </span> + Follow</button>
                             </div>
                         </a>
                     </div>
@@ -457,11 +461,8 @@ include('function.php');
                     </div>
 
                     <div class="flex justify-between items-center text-gray-400 text-sm">
-                        <div class="flex items-center space-x-4">
-                            <span class="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
-                                <?php echo $article['followers']; ?>
-                            </span>
+                        <div class="flex items-end space-x-4">
+                            
                             <span class="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" /></svg>
                                 <?php echo $article['upvotes']; ?>
