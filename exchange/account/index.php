de6e69fcf1bfd2f474931ea83e69bea40ab09214
Michael <eramehmichael2000@gmail.com>
2025-09-24 23:40:06 +0100

    update:

--- exchange/account/index.php
+++ exchange/account/index.php
@@ -634,6 +634,10 @@ $USDTfetch = mysqli_fetch_array($USDTquery);
                         contentToShow = depositContent;
                         tabToActivate = document.getElementById('deposit-tab');
                         break;
+                    case 'deposit-tab2':
+                        contentToShow = depositContent;
+                        tabToActivate = document.getElementById('deposit-tab');
+                        break;
                 }
 
                 if (contentToShow && tabToActivate) {
