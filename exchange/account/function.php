e8b12331f33861653aedfb3a2c56fc464395d35d
Michael <eramehmichael2000@gmail.com>
2025-09-24 23:38:05 +0100

    update:

--- exchange/account/function.php
+++ exchange/account/function.php
@@ -1,12 +1,12 @@
 <?php 
-include("connection.php");
+
 session_start();
 ob_start();
 
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 if(empty($_SESSION['user_id'])){
     header("location: ../../login.php"); 
